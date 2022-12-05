739bf778795c429db4a9f07506f790bfc0d59b37
edmundchan70 <edmundchan70@gmail.com>
2022-12-05 15:59:06 -0500

    updaed

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const ports = process.env.PORT || 8080;
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/db_method" , db_method);
+
 app.listen(ports , ()=>{
     console.log('listening on port'+ports);
 })  
